84aa4e931a91f03c7596257f0051025cd51b97c9
rvanderwall <rvanderwall@yahoo.com>
2021-12-22 16:33:26 -0500

    Add more info to readme and music.h

--- MIDI.ino
+++ MIDI.ino
@@ -1,30 +1,45 @@
-
 #include <MIDI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
+//
+// MIDI 
+//
 MIDI_CREATE_DEFAULT_INSTANCE();
+int LISTEN_CHANNEL = 4;
+int LISTEN_CHANNEL = MIDI_CHANNEL_OFF;  // Don't listen
+int SEND_CHANNEL = 1;
+int VELOCITY = 127;
 
+
+//
+// OLED Display screen
+//
 #define SCREEN_WIDTH 128 // OLED display width,  in pixels
 #define SCREEN_HEIGHT 32 // OLED display height, in pixels
 
+// https://arduinogetstarted.com/tutorials/arduino-oled
 // declare an SSD1306 display object connected to I2C
 Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
+
+//
+// Hardware mapping
+//
 int ON_BOARD_LED = 13;
 int PANEL_LED = 10;
 int BUTTON = 8;
 int SPEED = A0;
 
 
-int LISTEN_CHANNEL = 4;
-int SEND_CHANNEL = 1;
-int VELOCITY = 127;
-int NOTE = 60;
-
+//
+// MUSIC variables
+//
+int ROOT_NOTE = 60;
 int minor_chord[] = {0, 3, 7, 10};
 int chord_index = 0;
+
 
 void setup()
 {
@@ -33,23 +48,33 @@ void setup()
     pinMode(SPEED, INPUT);
 
     pinMode(BUTTON, INPUT_PULLUP);
-//    MIDI.begin(LISTEN_CHANNEL);                      // Launch MIDI and listen to channel
-    MIDI.begin(MIDI_CHANNEL_OFF);                      // Don't listen
+    MIDI.begin(LISTEN_CHANNEL);
 
     if (!oled.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
       Serial.println(F("SSD1306 allocation failed"));
       blink_error(3);
     }
-  delay(2000);         // wait for initializing
-  oled.clearDisplay(); // clear display
-  // https://arduinogetstarted.com/tutorials/arduino-oled
-  
-  oled.setTextSize(1);          // text size
-  oled.setTextColor(WHITE);     // text color
-  oled.setCursor(0, 10);        // position to display
-  oled.println("Hello World!"); // text to display
-  oled.display();               // show on OLED
+
+    delay(2000);         // wait for initializing
+    display_text("MIDI Sequencer", null);
 }
+
+
+void display_text(char *line1, char *line2) {
+    oled.clearDisplay(); // clear display
+    oled.setTextSize(1);         // text size
+    oled.setTextColor(WHITE);    // text color
+    if (line1 != null) {
+        oled.setCursor(0, 10);   // position to display
+        oled.println(line1);     // text to display
+    }
+    if (line2 != null) {
+        oled.setCursor(0, 20);   // position to display
+        oled.println(line2);     // text to display
+    }
+    oled.display();              // show on OLED
+}
+
 
 void blink_error(int err_code) {
   while (true){
@@ -63,19 +88,20 @@ void blink_error(int err_code) {
   }
 }
 
-void blink() {
+
+void pulse() {
     digitalWrite(ON_BOARD_LED, LOW);
     digitalWrite(PANEL_LED, HIGH);
-    delay(20);
+    delay(50);
     digitalWrite(ON_BOARD_LED, HIGH);
     digitalWrite(PANEL_LED, LOW);
-    delay(20);
+    delay(50);
 }
 
 
 void ping_moog() {
     digitalWrite(ON_BOARD_LED, HIGH);
-    int play_note = NOTE + minor_chord[chord_index];
+    int play_note = ROOT_NOTE + minor_chord[chord_index];
     chord_index++;
     if (chord_index > 3) {
       chord_index = 0;
@@ -83,27 +109,24 @@ void ping_moog() {
 
     int delay_time = 1024 - analogRead(SPEED);
     char buffer[128];
-  sprintf(buffer, "got delay = %d", delay_time);
-  oled.clearDisplay();
-  oled.setCursor(0, 20);        // position to display
-  oled.println(buffer); // text to display
-  oled.display();               // show on OLED
+    sprintf(buffer, "got delay = %d", delay_time);
+    display_text(null, buffer);
     
-    MIDI.sendNoteOn(play_note, VELOCITY, SEND_CHANNEL);   // Send a Note
-    delay(delay_time);                                     // Wait for a second
-    MIDI.sendNoteOff(play_note, 0, SEND_CHANNEL);         // Stop the note
+    MIDI.sendNoteOn(play_note, VELOCITY, SEND_CHANNEL);  // Send a Note
+    delay(delay_time);                                   // Wait for a second
+    MIDI.sendNoteOff(play_note, 0, SEND_CHANNEL);        // Stop the note
     delay(delay_time);                                   // Wait for a second
     digitalWrite(ON_BOARD_LED, LOW);
 }
 
+
 void loop()
 {
-//    if (MIDI.read())                    // If we have received a message
-//    {
+//    if (MIDI.read()) {
 //      ping()
 //    }
+
     if (digitalRead(BUTTON)== HIGH) {
-//      blink();
-      ping_moog();
+        ping_moog();
     }
 }
--- README.md
+++ README.md
@@ -1,19 +1,48 @@
-# MIDI
-https://itp.nyu.edu/physcomp/labs/labs-serial-communication/lab-midi-output-using-an-arduino/
+# Arduino Sequencer
+This project is an arduino based arpeggiator/ sequencer.
 
-# Arduino core
-https://www.arduino.cc/en/Tutorial/Foundations
+## References
+### MIDI
+- https://itp.nyu.edu/physcomp/labs/labs-serial-communication/lab-midi-output-using-an-arduino/
 
-# Ardunio Midi Library
-https://www.arduinolibraries.info/libraries/midi-library
-https://github.com/FortySevenEffects/arduino_midi_library
-https://github.com/FortySevenEffects/arduino_midi_library/releases/tag/4.2
+### Arduino core
+- https://www.arduino.cc/en/Tutorial/Foundations
 
-# Ardunio Midi App
-https://www.arduino.cc/en/Tutorial/BuiltInExamples/Midi
-https://create.arduino.cc/projecthub/dra/arduino-midi-arpeggiator-3bd731
-https://create.arduino.cc/projecthub/primvla/cthulhinho-a-midi-sequencer-1ac877
+### Ardunio Midi Library
+- https://www.arduinolibraries.info/libraries/midi-library
+- https://github.com/FortySevenEffects/arduino_midi_library
+- https://github.com/FortySevenEffects/arduino_midi_library/releases/tag/4.2
 
-# Display
-https://arduinogetstarted.com/tutorials/arduino-oled
+### Ardunio Midi App
+- https://www.arduino.cc/en/Tutorial/BuiltInExamples/Midi
+- https://create.arduino.cc/projecthub/dra/arduino-midi-arpeggiator-3bd731
+- https://create.arduino.cc/projecthub/primvla/cthulhinho-a-midi-sequencer-1ac877
+
+### Display
+- https://arduinogetstarted.com/tutorials/arduino-oled
+
+## Operating Instructions
+### Mode - Alternate between the musical modes
+- ionian (Major)
+- dorian (minor)
+- phrygian
+- lydian
+- mixolydian
+- aeolian (Natural minor)
+- locrian
+### Speed - beats per minute
+### Key - Root of chords and arpeggios
+- C, C#, D, .. Bb, B
+### Select - choose arpeggio or sequencer
+- Arpeggio
+  - ARP-135:  Arpeggio on a triad
+  - ARP-1357: Arpeggio on 7th chord
+  - ARP-Scale: Arpeggio on all notes in the scale
+- Sequence
+  - Press 'Rec'
+  - Press notes in your sequence
+  - Press 'Rec' again to lock in sequence
+### Run
+- Press once to start arpeggio or sequence playback
+- Press again to stop
 
